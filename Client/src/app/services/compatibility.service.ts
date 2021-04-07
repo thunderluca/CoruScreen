e64ab18196f966cba3f410dbc68e77195c5a3da2
Luca Montanari <luca.montanari@manageddesigns.it>
2021-04-07 20:46:59 +0200

    Added Firefox 69+ to unsupported cases due to mediaDevices.enumerateDevices different behavior that does not allow to retrieve device lists

--- Client/src/app/services/compatibility.service.ts
+++ Client/src/app/services/compatibility.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import * as Bowser from 'bowser';
 import { RtcService } from './rtc.service';
 
 @Injectable({
@@ -10,12 +11,34 @@ export class CompatibilityService implements CanActivate {
   constructor(private router: Router, private rtc: RtcService) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-    const isWebRTCAvailable = this.rtc.isWebRTCAvailable();
+    const supportedBrowser = this.isSupportedBrowser();
+    if (!supportedBrowser) {
+      // Breaking change on navigator.mediaDevices.enumerateDevices(),
+      // cannot load anymore the entire list even without asking permissions.
+      // See https://developer.mozilla.org/en-US/docs/Mozilla/Firefox/Releases/69
+      this.router.navigate(['notsupported']);
+      return false;
+    }
 
+    const isWebRTCAvailable = this.rtc.isWebRTCAvailable();
     if (!isWebRTCAvailable) {
       this.router.navigate(['notsupported']);
     }
 
     return isWebRTCAvailable;
+  }
+
+  private isSupportedBrowser(): boolean {
+    const browser = Bowser.getParser(navigator.userAgent);
+
+    const unsupportedFirefoxEdition = browser.getBrowserName().toLowerCase() === 'firefox' && browser.getBrowserVersion() >= '69';
+    if (unsupportedFirefoxEdition) {
+      // Breaking change on navigator.mediaDevices.enumerateDevices(),
+      // cannot load anymore the entire list even without asking permissions.
+      // See https://developer.mozilla.org/en-US/docs/Mozilla/Firefox/Releases/69
+      return false;
+    }
+
+    return true;
   }
 }
