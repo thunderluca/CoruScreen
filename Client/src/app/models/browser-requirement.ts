af9dcbc99be29c8cd88f76b7d53ceeb8e1b39f12
Luca Montanari <luca.montanari@manageddesigns.it>
2021-04-08 11:59:15 +0200

    Fixed browser requirements

--- Client/src/app/models/browser-requirement.ts
+++ Client/src/app/models/browser-requirement.ts
@@ -13,8 +13,8 @@ export class BrowserRequirement {
         // Coruscreen UX
         { displayName: 'Google Chrome', feature: 'app', names: ['chrome'], operatingSystems: [OperatingSystem.ChromeOS, OperatingSystem.Linux, OperatingSystem.MacOS, OperatingSystem.Windows], platforms: [DevicePlatform.Desktop], version: '56' },
         { displayName: 'Google Chrome', feature: 'app', names: ['chrome'], operatingSystems: [OperatingSystem.Android], platforms: [DevicePlatform.Mobile, DevicePlatform.Tablet, DevicePlatform.TV], version: '89' },
-        { displayName: 'Microsoft Edge', feature: 'app', names: ['edge', 'microsoft edge'], operatingSystems: [OperatingSystem.Android, OperatingSystem.Linux, OperatingSystem.MacOS, OperatingSystem.Windows], platforms: [DevicePlatform.Any], version: '79' },
-        { displayName: 'Mozilla Firefox', feature: 'app', names: ['firefox', 'mozilla firefox'], operatingSystems: [OperatingSystem.Android], platforms: [DevicePlatform.Mobile, DevicePlatform.Tablet, DevicePlatform.TV], version: '86' },
+        { displayName: 'Microsoft Edge', feature: 'app', names: ['edge', 'microsoft edge'], operatingSystems: [OperatingSystem.Linux, OperatingSystem.MacOS, OperatingSystem.Windows], platforms: [DevicePlatform.Any], version: '79' },
+        { displayName: 'Microsoft Edge', feature: 'app', names: ['edge', 'microsoft edge'], operatingSystems: [OperatingSystem.Android], platforms: [DevicePlatform.Mobile, DevicePlatform.Tablet, DevicePlatform.TV], version: '46' },
         { displayName: 'Opera Browser', feature: 'app', names: ['opera'], operatingSystems: [OperatingSystem.ChromeOS, OperatingSystem.Linux, OperatingSystem.MacOS, OperatingSystem.Windows], platforms: [DevicePlatform.Desktop], version: '43'  },
         // Permissions API
         { displayName: 'Google Chrome', feature: 'permissions', names: ['chrome'], operatingSystems: [OperatingSystem.ChromeOS, OperatingSystem.Linux, OperatingSystem.MacOS, OperatingSystem.Windows], platforms: [DevicePlatform.Desktop], version: '43' },
