53241cfb994f4230131bc44b99cbcbb09af890be
Luca Montanari <luca.montanari@manageddesigns.it>
2021-07-13 09:53:31 +0200

    HealthController added to Server project

---
+++ Server/Controllers/HealthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoruScreen.Server.Controllers
+{
+    public class HealthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Ok(new { status = "OK" });
+        }
+    }
+}
