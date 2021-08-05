250030d437074e9650a4c89170e86bb59eca2012
Luca Montanari <luca.montanari@manageddesigns.it>
2021-08-05 11:48:40 +0200

    Server program refactored using new ASP.NET Core minimal APIs

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,20 +1,50 @@
-using Microsoft.AspNetCore.Hosting;
+using CoruScreen.Server.Hubs;
+using CoruScreen.Server.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 
-namespace CoruScreen.Server
+var builder = WebApplication.CreateBuilder(args);
+
+var corsPolicy = builder.Configuration.GetSection("CorsPolicy").Get<CorsPolicy>();
+if (corsPolicy != null)
 {
-    public class Program
+    builder.Services.AddCors(options =>
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        options.AddPolicy(corsPolicy.Name, builder => builder.BuildUponPolicy(corsPolicy));
+    });
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    Console.WriteLine($"Cors policy '{corsPolicy.Name}' successfully registered");
 }
+
+builder.Services.AddControllers();
+builder.Services.AddSignalR();
+
+await using var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+var corsPolicyName = app.Configuration["CorsPolicy:Name"];
+if (!string.IsNullOrWhiteSpace(corsPolicyName))
+{
+    app.UseCors(corsPolicyName);
+}
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+    endpoints.MapHub<StreamHub>("/stream");
+});
+
+await app.RunAsync();
